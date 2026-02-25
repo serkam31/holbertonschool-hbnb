5304716d1e3d0cb1b04b5d1c224ad3ab60ba8f39
nobulp <lucaslupon873@gmail.com>
2026-02-25 14:48:06 +0100

    feat: implement user registration and retrieval endpoints in the users namespace

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,19 +1,27 @@
 from app.persistence.repository import InMemoryRepository
-
+from app.models.user import User
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    def get_users(self):
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, data):
+        user = self.get_user(user_id)
+        if not user:
+            return None
+        self.user_repo.update(user_id, data)
+        return self.get_user(user_id)

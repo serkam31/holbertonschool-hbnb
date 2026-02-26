9da7dc8083de701c36caf9c27baeb7e3e38c71c2
nobulp <lucaslupon873@gmail.com>
2026-02-26 10:20:35 +0100

    fix: correct Flask app initialization and ensure places namespace is included

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_restx import Api
 
+
 def create_app():
-    app = Flask(__name__)
+    app = Flask(name)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     from app.api.v1.users import api as users_ns
     from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
 
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
 
     return app

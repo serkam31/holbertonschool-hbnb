e11031fe386d0a3de67099241dc8c0411273bd02
nobulp <lucaslupon873@gmail.com>
2026-02-26 14:12:22 +0100

    fix: correct Flask app initialization by using __name__ for app instance

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -3,7 +3,7 @@ from flask_restx import Api
 
 
 def create_app():
-    app = Flask(name)
+    app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     from app.api.v1.users import api as users_ns

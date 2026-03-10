733e0283458cadf5586d4c3e3eb7a639e4021aa1
nobulp <lucaslupon873@gmail.com>
2026-03-10 11:25:14 +0100

    fix: Removing addition in __init__.py

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -5,6 +5,7 @@ from flask_restx import Api
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/')
+    
 
     from app.api.v1.users import api as users_ns
     from app.api.v1.amenities import api as amenities_ns

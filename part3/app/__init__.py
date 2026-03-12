648c125edd51e6b4ae325f866e267963155f0457
nobulp <lucaslupon873@gmail.com>
2026-03-12 10:52:33 +0100

    feat: Add authentication namespace with login endpoint and JWT token generation

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from app.api.v1.auth import api as auth_ns
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -10,6 +13,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     app.config.from_object(config_class)
 
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     from app.api.v1.users import api as users_ns
     from app.api.v1.amenities import api as amenities_ns
@@ -20,5 +24,6 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1')
 
     return app

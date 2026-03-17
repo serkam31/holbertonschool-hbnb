84576a1cd5d1eabdebc0fbe0efaaf820f96bdfb6
nobulp <lucaslupon873@gmail.com>
2026-03-17 10:58:02 +0100

    feat: add initial schema and insert data for users and amenities

---
+++ part3/insert_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (id, first_name, last_name, email, password, is_admin)
+VALUES (
+    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
+    'Admin',
+    'HBnB',
+    'admin@hbnb.io',
+    '$2b$12$seRF5EX5FYHNNxzm3N.h1e.e9ljbhz2iYSlxYgXhDgEooqT6auvaC',
+    TRUE
+);
+
+INSERT INTO amenities (id, name) VALUES
+('310cde4e-8a87-4f96-a6ce-06d413177017', 'WiFi'),
+('66c365c2-fb54-47aa-8826-3c276a6d72db', 'Swimming Pool'),
+('3d452507-1224-4655-ae9e-5db1c58847ce', 'Air Conditioning');
---
+++ part3/schema.sql
@@ -0,0 +1,44 @@
+CREATE TABLE users (
+    id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE places (
+    id CHAR(36) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL,
+    owner_id CHAR(36) NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES users(id)
+);
+
+CREATE TABLE reviews (
+    id CHAR(36) PRIMARY KEY,
+    text TEXT NOT NULL,
+    rating INT NOT NULL,
+    user_id CHAR(36) NOT NULL,
+    place_id CHAR(36) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (place_id) REFERENCES places(id),
+    CONSTRAINT chk_rating CHECK (rating >= 1 AND rating <= 5),
+    CONSTRAINT unique_user_place_review UNIQUE (user_id, place_id)
+);
+
+CREATE TABLE amenities (
+    id CHAR(36) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE place_amenity (
+    place_id CHAR(36) NOT NULL,
+    amenity_id CHAR(36) NOT NULL,
+    PRIMARY KEY (place_id, amenity_id),
+    FOREIGN KEY (place_id) REFERENCES places(id),
+    FOREIGN KEY (amenity_id) REFERENCES amenities(id)
+);
